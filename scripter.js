0ded0c71705a7d087cead9bf1dd31f90d649e1d5
Glitch (mulberry-delirious-close) <none>
2022-03-13 21:20:45 +0000

    ☂️🕵 Checkpoint ./scripter.js:35766351/6882 ./support.html:35766351/518

--- scripter.js
+++ scripter.js
@@ -3,16 +3,16 @@
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA6tS3GCf5S2O86sAZG-UpnWGFw_zzgb2Y",
-  authDomain: "alt1-3984d.firebaseapp.com",
-  databaseURL: "https://alt1-3984d-default-rtdb.firebaseio.com",
-  projectId: "alt1-3984d",
-  storageBucket: "alt1-3984d.appspot.com",
-  messagingSenderId: "754773598857",
-  appId: "1:754773598857:web:c24d797b66a0bc11bb5c58",
-  measurementId: "G-YJL677X91R",
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyCS9haigEMVHKxfMuAYZXtLtInUu8Oi4qQ",
+    authDomain: "alt1-732a0.firebaseapp.com",
+    projectId: "alt1-732a0",
+    storageBucket: "alt1-732a0.appspot.com",
+    messagingSenderId: "358627288961",
+    appId: "1:358627288961:web:5d0d5d5103aba16d46aa80",
+    measurementId: "G-T07CRQZ26C"
+  };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -20,42 +20,33 @@ firebase.initializeApp(firebaseConfig);
 const myDB = firebase.database().ref("/users");
 
 const btn2 = document.getElementById("submit");
-btn2.addEventListener("click", insertRecord);
+btn2.addEventListener("click",insertRecord);
 
 function insertRecord() {
   alert("Submit clicked!");
-
+  
   const nameBox = document.getElementById("fName");
   const nameBoxValue = nameBox.value;
   nameBox.value = "";
   nameBox.focus();
-
+  
   const surnameBox = document.getElementById("sName");
   const surnameBoxValue = surnameBox.value;
   surnameBox.value = "";
   surnameBox.focus();
-
-  const ageBox = document.getElementById("age");
-  const ageBoxValue = ageBox.value;
-  ageBox.value = "";
-  ageBox.focus();
-
+  
   const emailBox = document.getElementById("email");
   const emailBoxValue = emailBox.value;
   emailBox.value = "";
   emailBox.focus();
-
+  
   const commentBox = document.getElementById("comment");
   const commentBoxValue = commentBox.value;
   commentBox.value = "";
   commentBox.focus();
 
-  const data = myDB.push();
-  data.set({
-    name: nameBoxValue,
-    surname: surnameBoxValue,
-    age: ageBoxValue,
-    email: emailBoxValue,
-    comment: commentBoxValue,
-  });
+
+const data = myDB.push();
+data.set({name: nameBoxValue,surname: surnameBoxValue,email:emailBoxValue,comment: commentBoxValue})
+
 }
