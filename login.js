ca4adf2e79e5ff4fd70562dbd224a312f40654e2
Glitch (mulberry-delirious-close) <none>
2022-03-27 20:46:24 +0000

    🐙👲 Checkpoint ./login.js:35766351/2 ./.glitch-assets:35766351/1315 ./index.html:35766351/614

--- login.js
+++ login.js
@@ -3,7 +3,7 @@
       (document.getElementById("password").value == "password") & //gets the data inputted by the user and checks if they match the specified input
       (document.getElementById("username").value == "sample@gmail.com")
     ) {
-      window.location.href = "account.html"; //if right send user to account
+      window.location.href = "account.html"; //if right send user to account.html
     } else { //if its wrong send alert
       {
         alert("wrong keyword entry");
